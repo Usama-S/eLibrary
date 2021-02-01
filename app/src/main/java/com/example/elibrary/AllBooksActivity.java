7770c42a885d239ea10b8999a84adc512d2814a8
Usama-S <usamasultan.pk@gmail.com>
2021-02-01 12:24:00 +0500

    Added All Books Activity

---
+++ app/src/main/java/com/example/elibrary/AllBooksActivity.java
@@ -0,0 +1,34 @@
+package com.example.elibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class AllBooksActivity extends AppCompatActivity {
+
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, 824, "Computer Science", "John Maxwell",
+                "https://images-na.ssl-images-amazon.com/images/I/41MIIDToqhL.jpg",
+                "A Computer Science book.", "Long Description"));
+
+        adapter.setBooks(books);
+    }
+}
--- app/src/main/java/com/example/elibrary/MainActivity.java
+++ app/src/main/java/com/example/elibrary/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.elibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnCurrentlyReading, btnAlreadyRead, btnWishlist, btnFavourites, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViewItems();
+
+        btnAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AllBooksActivity.class));
+            }
+        });
+    }
+
+    private void initViewItems() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWishlist = findViewById(R.id.btnWishlist);
+        btnFavourites = findViewById(R.id.btnFavourites);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
