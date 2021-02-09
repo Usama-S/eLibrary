b0ac0c7d966a45a934e625ffe741d6b08e53633f
Usama-S <usamasultan.pk@gmail.com>
2021-02-09 16:53:37 +0500

    Added about button functionality | Added WebView Activity

--- app/src/main/java/com/example/elibrary/MainActivity.java
+++ app/src/main/java/com/example/elibrary/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.elibrary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -58,7 +61,25 @@ public class MainActivity extends AppCompatActivity {
         btnAbout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: 2/7/2021 show the dialogue box for about info 
+                /**
+                 * Showing the dialog box with the details about the app and the developer
+                 */
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("About")
+                        .setMessage("Designed and Developed for practice by Usama.\nVisist us for more details.")
+                        .setPositiveButton("Visit Us", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startActivity(new Intent(MainActivity.this, WebViewActivity.class));
+                            }
+                        })
+                        .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //nothing to do.
+                            }
+                        })
+                        .create().show();
             }
         });
     }
---
+++ app/src/main/java/com/example/elibrary/WebViewActivity.java
@@ -0,0 +1,30 @@
+package com.example.elibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView = findViewById(R.id.webView);
+        webView.loadUrl("https://www.google.com/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
